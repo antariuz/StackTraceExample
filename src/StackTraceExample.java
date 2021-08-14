062dac4814800dd1094babfc6891db6d35765f8f
Tymur Foshch <antariuz@gmail.com>
2021-08-14 09:49:11 +0300

    added MyException class

--- src/StackTraceExample.java
+++ src/StackTraceExample.java
@@ -1,10 +1,10 @@
 public class StackTraceExample {
-    public static void main(String[] args) {
-        System.out.println(convertStringToInt(null));
+    public static void main(String[] args) throws MyException {
+        System.out.println(convertStringToInt("null"));
     }
 
-    public static int convertStringToInt(String s) {
-        int x = Integer.parseInt(s);
-        return x;
+    public static int convertStringToInt(String s) throws MyException {
+        if (s.equals("")) throw new MyException("MyException message");
+        return Integer.parseInt(s);
     }
 }
